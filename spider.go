ec7a7bf3a617514c81b12898628b63d5131aa341
littlesuper <littlesuper@gmail.com>
2013-11-16 11:41:26 +0800

    实现从文本获取域名列表，并进行源码扫描

--- spider.go
+++ spider.go
@@ -4,38 +4,58 @@ import(
     "fmt"
     "regexp"
     "io/ioutil"
+    "io"
     "net/http"
     "strings"
     iconv "github.com/djimenez/iconv-go"
+    "os"
+    "bufio"
 )
 
 
-type myRegexp struct{ 
-    *regexp.Regexp
-}
- 
-func(r *myRegexp)FindStringSubmatchMap(s string) map[string]string{
-    captures:=make(map[string]string)
- 
-    match:=r.FindStringSubmatch(s)
-    if match==nil{
-        return captures
+var (
+    url string
+    src string
+    err error
+)
+
+func main(){
+
+    //逐行读取域名列表
+    f , err := os.Open("dm.txt")
+    if err != nil{
+       fmt.Printf("%v\n",err)
+       os.Exit(1)
     }
- 
-    for i,name:=range r.SubexpNames(){
-        if i==0||name==""{
-            continue
+
+   defer f.Close()
+   r := bufio.NewReader(f)
+   
+   //调用spider获取源码
+   for {
+        line, _, err := r.ReadLine()
+
+        if err == io.EOF {
+            fmt.Println("Spider end.")
+            break
+        } else if err != nil {
+            fmt.Println("Error:", err)
+            break
         }
-         captures[name]=match[i]
- 
-    }
-    return captures
+        src, err = spider("http://" + string(line))
+        //fmt.Println(string(line)) 
+        //fmt.Println(src)
+   }
+
+
 }
 
-func main() {
+
+//页面爬虫
+func spider(url string)(src string, err error) {
 
     //获取源码
-    url := "http://www.163.com/"
+    //url := "http://www.163.com/"
 
     resp, err := http.Get(url)
 
@@ -48,15 +68,15 @@ func main() {
         fmt.Println("http read error")
     }
 
-    src := string(body)
+    src = string(body)
 
 
     //获取编码
-	//src = `<html><head><meta charset="gb2312"></head><body>abc</body></html>`
-	var myExp = myRegexp{regexp.MustCompile(`charset="(?P<c>[^\"]*)`)}
-	re1 := myExp.FindStringSubmatchMap(src)
-	charset := re1["c"]
-	fmt.Println(charset)
+    //src = `<html><head><meta charset="gb2312"></head><body>abc</body></html>`
+    var myExp = myRegexp{regexp.MustCompile(`charset="(?P<c>[^\"]*)`)}
+    re1 := myExp.FindStringSubmatchMap(src)
+    charset := re1["c"]
+    //fmt.Println(charset)
 
     //转换编码
     src,_ = iconv.ConvertString(src, charset, "utf-8")
@@ -89,9 +109,31 @@ func main() {
     re, _ = regexp.Compile("\\&[l|g]t;")
     src = re.ReplaceAllString(src, "")
 
-    fmt.Println(src)
+    //fmt.Println(src)
+    return
 
 }
 
 
-
+//正则扩展，用于爬虫获取页面编码
+type myRegexp struct{ 
+    *regexp.Regexp
+}
+ 
+func(r *myRegexp)FindStringSubmatchMap(s string) map[string]string{
+    captures:=make(map[string]string)
+ 
+    match:=r.FindStringSubmatch(s)
+    if match==nil{
+        return captures
+    }
+ 
+    for i,name:=range r.SubexpNames(){
+        if i==0||name==""{
+            continue
+        }
+         captures[name]=match[i]
+ 
+    }
+    return captures
+}
